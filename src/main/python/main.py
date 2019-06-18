c1af3ae7460eaf041d52388bc74368edbd0b80dd
sstucker <sstucker@bu.edu>
2019-06-18 17:59:28 -0400

    Added control groupbox

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,4 +1,5 @@
-from fbs_runtime.application_context import ApplicationContext
+import PyQt5
+from fbs_runtime.application_context.PyQt5 import ApplicationContext
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QWidget
 from PyQt5.QtWidgets import QLabel
@@ -22,6 +23,12 @@ from PyQt5 import QtCore
 
 QtInstance = QtCore.QCoreApplication.instance()
 
+if hasattr(QtCore.Qt, 'AA_EnableHighDpiScaling'):
+    PyQt5.QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
+
+if hasattr(QtCore.Qt, 'AA_UseHighDpiPixmaps'):
+    PyQt5.QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True)
+
 if QtInstance is None:
     QtInstance = QApplication(sys.argv)
 
@@ -31,13 +38,13 @@ class Main(QTabWidget):
 
         super().__init__(parent)
 
-        self.tab0 = QWidget()
+        # self.tab0 = QWidget()
         self.tabFigEight = TabFigEight(parent=self)
 
-        self.addTab(self.tab0,'Tab 0')
+        # self.addTab(self.tab0,'Tab 0')
         self.addTab(self.tabFigEight,'Figure-8 Motion Quantification')
         
-        self.windowTitle = 'MinaWare V.0.0.1'
+        self.windowTitle = 'PyOCT V.0.0.1'
         self.setWindowTitle(self.windowTitle)
     
         
@@ -51,16 +58,17 @@ class TabFigEight(QWidget):
         self.setLayout(self.mainGrid)
         
         self.file = FileGroupbox('File')
-        self.mainGrid.addWidget(self.file,1,1,1,3)
+        self.mainGrid.addWidget(self.file,1,2)
+
+        self.control = ControlGroupbox('Control')
+        self.mainGrid.addWidget(self.control,2,2)
         
         #PLOTS
         self.plotSpectrum = PyQtG.PlotWidget(name="Spectral Scan")
-        self.mainGrid.addWidget(self.plotSpectrum,1,0,1,1)
-        self.plotSpectrum.setFixedWidth(500)
+        self.mainGrid.addWidget(self.plotSpectrum,1,1)
                 
         self.plotIFFT = PyQtG.PlotWidget(name="IFFT")
-        self.mainGrid.addWidget(self.plotIFFT,2,0,1,1)
-        self.plotIFFT.setFixedWidth(500)
+        self.mainGrid.addWidget(self.plotIFFT,2,1)
 
         
 class FileGroupbox(QGroupBox):
@@ -70,30 +78,56 @@ class FileGroupbox(QGroupBox):
         super().__init__(name)
             
         self.layout = QFormLayout()
+
+        self.setFixedWidth(700)
         
         self.formFile = QGroupBox("File")
         self.fileLayout = QFormLayout()
     
         self.entryExpName = QLineEdit()
+        now = time.strftime("%y-%m-%d")
+        default = now+'-PyImageOCT-exp'
+        self.entryExpName.setText(default)
+
         self.entryExpDir = QLineEdit()
+        here = str(Path.home())+'\\PyImageOCT\\Experiments\\'+default
+        self.entryExpDir.setText(here)
+
         self.entryFileSize = QComboBox()
         self.entryFileSize.addItems(["250 MB","500 MB","1 GB"])
+
         self.entryFileType = QComboBox()
         self.entryFileType.addItems([".npy",".csv"])
+
         self.layout.addRow(QLabel("Experiment name"),self.entryExpName)
-        now = time.strftime("%Y%m%d-%H%M%S")
-        self.entryExpName.setText(now+'-PyOCT-experiment')
         self.layout.addRow(QLabel("Experiment directory"),self.entryExpDir)
-        self.entryExpDir.setText(str(Path.home()))
         self.layout.addRow(QLabel("Maximum file size"),self.entryFileSize)
         self.layout.addRow(QLabel("File type"),self.entryFileType)
         
         self.setLayout(self.layout)
 
+class ControlGroupbox(QGroupBox):
+
+    def __init__(self, name, parent=None):
+
+        super().__init__(name)
+
+        self.layout = QGridLayout()
+
+        self.scanButton = QPushButton('SCAN')
+        self.acqButton = QPushButton('ACQUIRE')
+        self.stopButton = QPushButton('STOP')
+
+        self.layout.addWidget(self.scanButton,0,0)
+        self.layout.addWidget(self.acqButton,0,1)
+        self.layout.addWidget(self.stopButton,0,2)
+
+        self.setLayout(self.layout)
+
 if __name__ == '__main__':
-    appctxt = ApplicationContext()       # 1. Instantiate ApplicationContext
+    appctxt = ApplicationContext()
     window = Main()
-    window.resize(1000, 600)
+    window.resize(1200, 600)
     window.show()
-    exit_code = appctxt.app.exec_()      # 2. Invoke appctxt.app.exec_()
+    exit_code = appctxt.app.exec_()
     sys.exit(exit_code)
