f5d351af63e388c002041770cbce42ac72d831d4
sstucker <sstucker@bu.edu>
2019-06-19 19:27:34 -0400

    Working deque and threading

--- src/main/python/PyImage/SpectralRadarControl.py
+++ src/main/python/PyImage/SpectralRadarControl.py
@@ -1,6 +1,7 @@
 import PySpectralRadar as SpectralRadar
 import numpy as np
-from PyQt5.QtCore import QThread
+import collections
+from PyQt5.QtCore import QObject, QThread, pyqtSignal, pyqtSlot
 from PyImage.OCT import *
 
 class FigureEight:
@@ -13,11 +14,20 @@ class FigureEight:
         self.liveY = np.empty(1024)
 
         self.__RUNNING__ = True
+        self.__RAW__ = collections.deque()
 
     def initScan(self):
+
         print('Init scan')
-        test = PyQtPlot2DThread(self)
-        test.start(self.plotWidget)
+        acquisitionThread = acqThread(self)
+
+        acq = Acquisition(self,0)
+        acq.moveToThread(acquisitionThread)
+        acquisitionThread.started.connect(acq.work)
+        acquisitionThread.start()
+
+        plotter = PyQtPlot2DThread(self)
+        plotter.start(self.plotWidget)
 
     def displayPattern(self):
         self.scatterWidget.plot(self.X, self.Y)
@@ -32,18 +42,45 @@ class FigureEight:
 class PyQtPlot2DThread(QThread):
 
     def __init__(self,controller):
-
         QThread.__init__(self)
-
         self.controller = controller
 
     def __del__(self):
         self.wait()
 
     def start(self,plotWidget):
-        for i in range(10000):
             while self.controller.__RUNNING__:
-                liveX = np.arange(1024)
-                liveY = np.random.randint(0, 2000, 1024)
-                plotWidget.plot(liveX, liveY)
+                if len(self.controller.__RAW__) > 5:
+                    Y = self.controller.__RAW__.popleft()
+                    self.controller.plotWidget.plot(np.arange(1024),Y)
+
+class acqThread(QThread):
+
+    def __init__(self,controller):
+        QThread.__init__(self)
+        self.controller = controller
+
+    def __del__(self):
+        self.wait()
+
+
+class Acquisition(QObject):
+
+    def __init__(self,controller,id: int):
+        super().__init__()
+        self.controller = controller
+        self.__id = id
+        self.__abort = False
+
+    @pyqtSlot()
+    def work(self):
+        thread_name = QThread.currentThread().objectName()
+        thread_id = int(QThread.currentThreadId())  # cast to int() is necessary
+        for i in range(5000):
+            while not self.__abort:
+                self.controller.__RAW__.append(np.random.randint(0,100,1024))
+
+    def abort(self):
+        self.__abort = True
+
 
