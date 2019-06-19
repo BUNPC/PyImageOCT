e03867a4cad90c2830331d7dc879fb2f852a635b
sstucker <sstucker@bu.edu>
2019-06-19 14:43:08 -0400

    Comments and file organization

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -26,25 +26,31 @@ from PyImage import Widgets
 
 QtInstance = QtCore.QCoreApplication.instance()
 
-# if hasattr(QtCore.Qt, 'AA_EnableHighDpiScaling'):
-#     PyQt5.QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
-#
-# if hasattr(QtCore.Qt, 'AA_UseHighDpiPixmaps'):
-#     PyQt5.QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True)
+#Something wrong with these-- intended to fix DPI scaling issues but they make it worse
+'''
+if hasattr(QtCore.Qt, 'AA_EnableHighDpiScaling'):
+    PyQt5.QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
 
+if hasattr(QtCore.Qt, 'AA_UseHighDpiPixmaps'):
+    PyQt5.QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True)
+'''
+
+#Allows IPython kernal to recover from closing of app
 if QtInstance is None:
     QtInstance = QApplication(sys.argv)
 
 class Main(QTabWidget):
+    '''
+    At the highest level, the GUI consists of tabs which organize various scanning/acquisition modes, similar to the
+    OCT Imaging Freeform SDK program developed in LabView
+    '''
     
     def __init__(self,parent=None):
 
         super().__init__(parent)
 
-        # self.tab0 = QWidget()
         self.tabFigEight = TabFigEight(parent=self)
 
-        # self.addTab(self.tab0,'Tab 0')
         self.addTab(self.tabFigEight,'Figure-8 Motion Quantification')
         
         self.windowTitle = 'PyOCT V.0.0.1'
@@ -52,29 +58,35 @@ class Main(QTabWidget):
     
         
 class TabFigEight(QWidget):
+    '''
+    Experimental scanning mode which uses figure-8 pattern to quickly measure 3D motion
+    along two orthogonal B-scans.
+    '''
     
     def __init__(self,parent=None):
         
         super().__init__(parent)
-        
+
         self.mainGrid = QGridLayout()
         self.setLayout(self.mainGrid)
-        
+
+        #File I/O properties interface
         self.file = Widgets.FileGroupbox('File')
         self.mainGrid.addWidget(self.file,1,2)
 
+        #Thorlabs SpectralRadar SDK is wrapped with PySpectralRadar module.
+        #Interfaces corresponding to scanning modes are in PyImage.SpectralRadarControl
         self.srcontrol = SpectralRadarControl.FigureEight()
 
+        #Control GUI which is slave of SpectralRadarController defined above
         self.control = Widgets.ControlGroupbox('Control',spectralRadarController=self.srcontrol)
         self.mainGrid.addWidget(self.control,2,2)
         
         #PLOTS
         self.plotSpectrum = PyQtG.PlotWidget(name="Spectral Scan")
         self.mainGrid.addWidget(self.plotSpectrum,1,1)
-                
-        self.plotIFFT = PyQtG.PlotWidget(name="IFFT")
-        self.mainGrid.addWidget(self.plotIFFT,2,1)
 
+#Qt main loop
 if __name__ == '__main__':
     appctxt = ApplicationContext()
     window = Main()
