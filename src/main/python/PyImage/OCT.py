SpectralRadar scan pattern rotate feature replaced with geometric rotation of positions. Introduced weird PyQtGraph PlotWidget axes scaling bug on some displays

src/main/python/PyImage/OCT.py
@@ -3,12 +3,13 @@ import numba
 from scipy.interpolate import interp1d
 
 
-def generateIdealFigureEightPositions(xsize, alinesPerX, rpt=1, flyback=20, flybackAngle=np.pi/2.58):
+def generateIdealFigureEightPositions(xdistance, alinesPerX, rpt=1, angle=np.pi / 4, flyback=20,flybackAngle=np.pi / 2.58):
     """
     Generates figure-8 scan pattern positions with orthogonal cross.
     :param xdistance: Distance between adjacent scans in perpendicular B-scans
     :param alinesPerX: Number of A-lines in each orthogonal B-scan
-    :param rpt: Number of times to repeat the pattern in the 1D positions array
+    :param rpt: Number of times to repeat the pattern in the 1D positions array. Default is 1
+    :param angle: Angle by which to rotate the figure-8 in radians. Default is pi/4
     :param flyback: Number of A-lines in each flyback loop
     :param flybackAngle: Range over which to sweep flyback loops in radians
     :return: posRpt: 1D positions array for use with FreeformScanPattern; [x1,y1,x2,y2...] format
@@ -19,6 +20,10 @@ def generateIdealFigureEightPositions(xsize, alinesPerX, rpt=1, flyback=20, flyb
              N: Total number of A-scans in the pattern
              D: Distance between adjacent A-scans in the B-scans
     """
+    fbscale = 2
+    xsize = np.sqrt(2) / 4 * xdistance * (alinesPerX - 1)
+    rotmat = np.array([[np.cos(angle), np.sin(angle)], [-np.sin(angle), np.cos(angle)]])
+
     if rpt > 0:
         cross = np.linspace(-xsize, xsize, alinesPerX)
 
@@ -30,10 +35,10 @@ def generateIdealFigureEightPositions(xsize, alinesPerX, rpt=1, flyback=20, flyb
 
         D = np.sqrt((B1[0][0] - B1[0][1]) ** 2 + (B1[1][0] - B1[1][1]) ** 2)
 
-        x1 = 2.95 * xsize * np.cos(fb1)
-        y1 = (1.572 * xsize) * np.sin(2 * fb1)
-        x2 = 2.95 * xsize * np.cos(fb2)
-        y2 = (1.572 * xsize) * np.sin(2 * fb2)
+        x1 = 1.93 * fbscale * xsize * np.cos(fb1)
+        y1 = fbscale * xsize * np.sin(2 * fb1)
+        x2 = 1.93 * fbscale * xsize * np.cos(fb2)
+        y2 = fbscale * xsize * np.sin(2 * fb2)
 
         X = np.concatenate([x1, B1[0], x2, B2[0]])
         Y = np.concatenate([y1, B1[1], y2, B2[1]])
@@ -50,11 +55,14 @@ def generateIdealFigureEightPositions(xsize, alinesPerX, rpt=1, flyback=20, flyb
         pos[0::2] = X
         pos[1::2] = Y
 
+        [X, Y] = np.matmul(rotmat, [X, Y])
+
         posRpt = np.tile(pos, rpt)
 
         N = len(X)
 
         return [posRpt, X, Y, b1, b2, N, D]
+
 
 @numba.jit(forceobj=True)
 def fig8ToBScan(A, N, B, AlinesPerX, apod, ROI=400, lam=None, start=14):

src/main/python/PyImage/Widgets.py
@@ -222,7 +222,7 @@ class Fig8GroupBox(QGroupBox):
 
         self.spinFlyback = QSpinBox()
         self.spinFlyback.setRange(2, 600)
-        self.spinFlyback.setValue(20)
+        self.spinFlyback.setValue(100)
         self.spinFlyback.valueChanged.connect(self.update)
 
         self.spinAngle = QSpinBox()
@@ -233,19 +233,19 @@ class Fig8GroupBox(QGroupBox):
 
         self.spinFlybackAngle = QDoubleSpinBox()
         self.spinFlybackAngle.setRange(40, 120)
-        self.spinFlybackAngle.setValue(69.5)
+        self.spinFlybackAngle.setValue(74.5)
         self.spinFlybackAngle.setSingleStep(0.5)
         self.spinFlybackAngle.setDecimals(1)
         self.spinFlybackAngle.setSuffix('°')
         self.spinFlybackAngle.valueChanged.connect(self.update)
 
-        self.spinFig8Size = QDoubleSpinBox()
-        self.spinFig8Size.setRange(0.00001, 3)
-        self.spinFig8Size.setSuffix(' mm')
-        self.spinFig8Size.setDecimals(6)
-        self.spinFig8Size.setSingleStep(0.00001)
-        self.spinFig8Size.setValue(0.08)
-        self.spinFig8Size.valueChanged.connect(self.update)
+        self.spinALineSpacing = QDoubleSpinBox()
+        self.spinALineSpacing.setRange(0.001, 2000)
+        self.spinALineSpacing.setSuffix(' um')
+        self.spinALineSpacing.setDecimals(2)
+        self.spinALineSpacing.setSingleStep(0.10)
+        self.spinALineSpacing.setValue(3.00)
+        self.spinALineSpacing.valueChanged.connect(self.update)
 
         self.spinFig8Total = QSpinBox()
         self.spinFig8Total.setRange(2, 5000)
@@ -256,12 +256,6 @@ class Fig8GroupBox(QGroupBox):
         self.spinAcqTime.setRange(10, 20000)
         self.spinAcqTime.setValue(1000)
         self.spinAcqTime.valueChanged.connect(self.update)
-
-        self.textDistance = QTextEdit()
-        self.textDistance.setReadOnly(True)
-        self.textDistance.setFixedHeight(24)
-        self.textDistance.setCursorWidth(0)
-        self.textDistance.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
 
         self.textRate = QTextEdit()
         self.textRate.setReadOnly(True)
@@ -277,10 +271,9 @@ class Fig8GroupBox(QGroupBox):
 
         self.layout.addRow(QLabel("A-lines per B-scan"), self.spinALinesPerX)
         self.layout.addRow(QLabel("A-lines per flyback"), self.spinFlyback)
-        self.layout.addRow(QLabel("Scan-pattern angle"), self.spinAngle)  # TODO fix rotation mechanic
+        self.layout.addRow(QLabel("Distance between A-lines in B-scan"), self.spinALineSpacing)
+        self.layout.addRow(QLabel("Scan-pattern angle"), self.spinAngle)
         self.layout.addRow(QLabel("Flyback angle"), self.spinFlybackAngle)
-        self.layout.addRow(QLabel("Figure-8 width"), self.spinFig8Size)
-        self.layout.addRow(QLabel("Distance between adjacent A-scans"), self.textDistance)
         self.layout.addRow(QLabel("Total A-scans in each figure-8"), self.textTotal)
         self.layout.addRow(QLabel("Rate of figure-8 acquisition"), self.textRate)
         self.layout.addRow(QLabel("Total Figure-8s to acquire"), self.spinFig8Total)
@@ -289,14 +282,13 @@ class Fig8GroupBox(QGroupBox):
         self.update()
 
     def update(self):
-        self.controller.setScanPatternParams(self.spinFig8Size.value(),
+        self.controller.setScanPatternParams(self.spinALineSpacing.value()*10**-3,  # Convert from um to mm
                                              self.spinALinesPerX.value(),
                                              self.spinFlyback.value(),
                                              self.spinFig8Total.value(),
-                                             self.spinAngle.value()*(np.pi/180),
-                                             self.spinFlybackAngle.value()*(np.pi/180)) # Conversion to rad happens here!
+                                             self.spinAngle.value() * (np.pi/180),
+                                             self.spinFlybackAngle.value() * (np.pi/180)) # Conversion to rad happens here!
 
-        self.textDistance.setText(str(self.controller.scanPatternD * 10 ** 6)[0:8] + ' nm')
         self.textTotal.setText(str(self.controller.scanPatternN))
 
         w = 1 / (1 / self.controller.getRateValue() * self.controller.scanPatternN)
@@ -306,7 +298,7 @@ class Fig8GroupBox(QGroupBox):
     def enabled(self, bool):
         self.spinALinesPerX.setEnabled(bool)
         self.spinFlyback.setEnabled(bool)
-        self.spinFig8Size.setEnabled(bool)
+        self.spinALineSpacing.setEnabled(bool)
         self.spinAngle.setEnabled(bool)
         self.spinFlybackAngle.setEnabled(bool)
         self.spinAcqTime.setEnabled(bool)