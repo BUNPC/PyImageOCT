Rotation GUI fixes

src/main/python/PyImage/OCT.py
@@ -43,6 +43,8 @@ def generateIdealFigureEightPositions(xdistance, alinesPerX, rpt=1, padB=0, angl
         X = np.concatenate([x1, B1[0], x2, B2[0]])
         Y = np.concatenate([y1, B1[1], y2, B2[1]])
 
+        [X, Y] = np.matmul(rotmat, [X, Y])
+
         b1 = np.concatenate(
             [np.zeros(flyback+padB), np.ones(alinesPerX-2*padB), np.zeros(flyback+padB), np.zeros(alinesPerX)]).astype(
             np.bool)
@@ -54,8 +56,6 @@ def generateIdealFigureEightPositions(xdistance, alinesPerX, rpt=1, padB=0, angl
 
         pos[0::2] = X
         pos[1::2] = Y
-
-        [X, Y] = np.matmul(rotmat, [X, Y])
 
         posRpt = np.tile(pos, rpt)
 

src/main/python/PyImage/SpectralRadarControl.py
@@ -332,6 +332,7 @@ class FigureEight:
                 spec = raw.flatten()[0:2048]  # First spectrum of the B-scan only is plotted
 
                 bscan = self.process8(raw, B, ROI=self._roi_z)
+                print(np.shape(bscan))
 
                 self.plotSpectrum.plot1D(spec)
                 self.plotBScan.update(20 * np.log10(np.abs(np.transpose(bscan))))
@@ -507,7 +508,7 @@ class FigureEight:
 
     def setScanPatternParams(self, patternSize, aLinesPerCross, bPadding, aLinesPerFlyback, repeats, patternAngle, flybackAngle):
         self._scanPatternSize = patternSize
-        self._scanPatternAlinesPerCross = aLinesPerCross
+        self._scanPatternAlinesPerCross = aLinesPerCross - 2 * bPadding  # Padding subtracted here
         self._scanPatternBPadding = bPadding
         self._scanPatternAlinesPerFlyback = aLinesPerFlyback
         self._scanPatternTotalRepeats = repeats