f65a60051174a0fcf9c3be09e7c8eba8cdcdfd48
sstucker <sstucker@bu.edu>
2020-01-16 12:25:38 -0500

    pyqtgraph widgets can be added to layout defined in xml. Project would probably be easier to organize if multiple layouts were defined and belonged to a class per tab?

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,21 +1,38 @@
 from fbs_runtime.application_context.PyQt5 import ApplicationContext
 from PyQt5.QtWidgets import QMainWindow
-from PyQt5 import uic
+from PyQt5 import uic, QtWidgets
+import pyqtgraph as pyqtg
+
 
 import sys
 import os
 
 class MainWindow(QMainWindow):
     def __init__(self):
-        super(MainWindow, self).__init__() # Call the inherited classes __init__ method
+        super(MainWindow, self).__init__()
         uifile = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))+"\\ui\\MainWindow.ui"
-        uic.loadUi(uifile,self) # Load the .ui file
+        uic.loadUi(uifile,self)  # Entire GUI is marked up in one .ui file
+
+        layout2D = self.findChild(QtWidgets.QGridLayout, 'layout2D')
+        layoutSpectrum = self.findChild(QtWidgets.QGridLayout, 'layoutSpectrum')
+
+        print(layout2D,layoutSpectrum)
+
+        self.plot2D = pyqtg.PlotWidget()
+        self.plotSpectrum = pyqtg.PlotWidget()
+
+        layout2D.addWidget(self.plot2D)
+        layoutSpectrum.addWidget(self.plotSpectrum)
+
         self.show()
+
+
 
 if __name__ == '__main__':
     appctxt = ApplicationContext()       # 1. Instantiate ApplicationContext
     window = MainWindow()
-    window.resize(250, 150)
+    window.resize(1200, 1080)
+    window.setMinimumSize(1200,1080)
     window.show()
     exit_code = appctxt.app.exec_()      # 2. Invoke appctxt.app.exec_()
     sys.exit(exit_code)
