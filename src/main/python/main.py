98775bbc23bba9e4662276bd0f0192f568aa3cd3
sstucker <sstucker@bu.edu>
2020-05-17 16:09:51 -0500

    Panels

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,8 +1,7 @@
 from fbs_runtime.application_context.PyQt5 import ApplicationContext
 from PyQt5.QtWidgets import QMainWindow, QWidget
-from PyQt5 import uic, QtWidgets
-from octacontroller import OCTAController
-
+from PyQt5 import uic
+from src.main.python.widgets.panels import FilePanel, RepeatsPanel, ConfigPanel, ScanPanelOCTA
 
 import sys
 import os
@@ -23,46 +22,13 @@ class MainWindow(QMainWindow):
 
         self.show()
 
-from PyQt5.QtWidgets import QGroupBox, QLineEdit, QComboBox, QToolButton, QFileDialog
-
-class FilePanel(QGroupBox):
-
-    def __init__(self):
-
-        super(QGroupBox, self).__init__()
-
-        ui = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))+"\\ui\\filePanel.ui"
-        uic.loadUi(ui, self)
-        self.show()
-
-        self.exp_dir_line = self.findChild(QLineEdit, "lineExp")
-        self.trial_name_line = self.findChild(QLineEdit, "lineFileName")
-        self.file_type_combo = self.findChild(QComboBox, "comboFileType")
-        self.file_size_combo = self.findChild(QComboBox, "comboFileSize")
-
-        self.file_browse_button = self.findChild(QToolButton, "buttonBrowse")
-        self.file_browse_button.clicked.connect(self.browse_for_file)
-
-        self.file_dialog = QFileDialog()
-
-        # Defaults
-
-        self.experiment_directory = None
-        self.trial_name = None
-        self.file_type = None
-        self.file_size = None
-
-    def browse_for_file(self):
-        #  Uses QFileDialog to select a directory to save in
-        self.experiment_directory = self.file_dialog.getExistingDirectory(self, "Select Directory")
-        self.exp_dir_line.setText(self.experiment_directory)
 
 if __name__ == '__main__':
     appctxt = ApplicationContext()       # 1. Instantiate ApplicationContext
-    window = FilePanel()
-    window.resize(860, 920)
-    window.setMinimumSize(860,920)
-    # window.setMaximumSize(860,920)
+    window = ScanPanelOCTA()
+    # window.resize(860, 920)
+    # window.setMinimumSize(860,920)
+    # # window.setMaximumSize(860,920)
     window.show()
     exit_code = appctxt.app.exec_()      # 2. Invoke appctxt.app.exec_()
     sys.exit(exit_code)
---
+++ src/main/python/widgets/panels.py
@@ -0,0 +1,246 @@
+from PyQt5 import uic
+from PyQt5.QtWidgets import QGroupBox, QLineEdit, QComboBox, QToolButton, QFileDialog, QSpinBox, QDoubleSpinBox, QCheckBox, QPushButton
+from datetime import datetime
+import os
+import glob
+
+FileTypes = [
+    ".npy",
+    ".mat"
+]
+
+
+FileSizes = [
+    "250 MB",
+    "500 MB",
+    "1 GB",
+    "2 GB",
+    "4 GB"
+]
+
+
+Rates = {
+    "76 kHz": 76000,
+    "146 kHz": 146000
+}
+
+
+class ScanPanelOCTA(QGroupBox):
+
+    def __init__(self):
+
+        super(QGroupBox, self).__init__()
+        ui = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))+"\\ui\\scan_octa.ui"  # Double escape dir
+        uic.loadUi(ui, self)
+
+        self.indefinite_check = self.findChild(QCheckBox, "checkIndefinite")
+        self.indefinite_check.stateChanged.connect(self.indefinite_check_changed)
+
+        self.equal_aspect_check = self.findChild(QCheckBox, "checkEqualAspect")
+        self.equal_aspect_check.stateChanged.connect(self.equal_aspect_check_changed)
+
+        self.scan_number_spin = self.findChild(QSpinBox, "spinScanNumber")
+
+        self.roi_x_spin = self.findChild(QDoubleSpinBox, "spinROIWidth")
+        self.x_count_spin = self.findChild(QSpinBox, "spinACount")
+        self.x_spacing_spin = self.findChild(QDoubleSpinBox, "spinFastAxisSpacing")
+
+        self.roi_y_spin = self.findChild(QDoubleSpinBox, "spinROIHeight")
+        self.y_count_spin = self.findChild(QSpinBox, "spinBCount")
+        self.y_spacing_spin = self.findChild(QDoubleSpinBox, "spinSlowAxisSpacing")
+
+        self.preview_button = self.findChild(QPushButton, "previewButton")
+
+        self.x_spacing_spin.valueChanged.connect(self.x_spacing_changed)
+
+        self.x_spacing_spin.mousePressEvent = self.setSpacingDriving
+        self.y_spacing_spin.mousePressEvent = self.setSpacingDriving
+
+        self.roi_x_spin.mousePressEvent = self.setROIDriving
+        self.roi_y_spin.mousePressEvent = self.setROIDriving
+
+        self.mousePressEvent = self.update_dimensions
+
+        self.SPACING_DRIVING = False
+        self.ROI_DRIVING = False
+
+        self.show()
+
+
+    def setSpacingDriving(self, event):
+        print("Set spacing driving")
+        self.SPACING_DRIVING = True
+        self.ROI_DRIVING = False
+
+    def setROIDriving(self, event):
+        print("Set ROI driving")
+        self.SPACING_DRIVING = False
+        self.ROI_DRIVING = True
+
+    def indefinite_check_changed(self):
+
+        if self.indefinite_check.isChecked():
+
+            self.scan_number_spin.setEnabled(False)
+
+        else:
+
+            self.scan_number_spin.setEnabled(True)
+
+    def equal_aspect_check_changed(self):
+
+        if self.equal_aspect_check.isChecked():
+
+            self.y_spacing_spin.setValue(self.x_spacing_spin.value())
+            self.y_spacing_spin.setEnabled(False)
+
+        else:
+
+            self.y_spacing_spin.setEnabled(True)
+
+    def x_spacing_changed(self):
+
+        if self.equal_aspect_check.isChecked():
+
+            self.y_spacing_spin.setValue(self.x_spacing_spin.value())
+
+    def update_dimensions(self, event):
+
+        if self.SPACING_DRIVING:
+            print("Updating roi re: spacing")
+            self.roi_x_spin.setValue( (self.x_spacing_spin.value() * self.x_count_spin.value()) / 1000)
+            self.roi_y_spin.setValue( (self.y_spacing_spin.value() * self.x_count_spin.value()) / 1000)
+
+        if self.ROI_DRIVING:
+            print("Updating spacing re: roi")
+            self.x_spacing_spin.setValue(
+                (self.roi_x_spin.value() * self.x_count_spin.value()) / self.x_spacing_spin.value())
+            self.y_spacing_spin.setValue(
+                (self.roi_y_spin.value() * self.x_count_spin.value()) / self.y_spacing_spin.value())
+
+class ConfigPanel(QGroupBox):
+
+    def __init__(self):
+
+        super(QGroupBox, self).__init__()
+        ui = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))+"\\ui\\config.ui"  # Double escape dir
+        uic.loadUi(ui, self)
+
+        self.configdir = "C:\\ThorLabs\\SpectralRadar\\config"  # TODO set this intelligently on startup
+        self.configpaths = glob.glob(self.configdir+"/*.ini")
+        print(self.configpaths)
+
+        self.config_combo = self.findChild(QComboBox, "comboConfig")
+        for path in self.configpaths:
+            self.config_combo.addItem(os.path.basename(path).split(".")[0])  # Just get name of file w/o extension
+
+        self.rate_combo = self.findChild(QComboBox, "comboRate")
+        self.apod_combo = self.findChild(QComboBox, "comboApodization")
+
+        self.bitness_check = self.findChild(QCheckBox, "check32")
+        self.fft_check = self.findChild(QCheckBox, "checkFFT")
+        self.interp_check = self.findChild(QCheckBox, "checkInterpolation")
+
+        self.show()
+
+    def get_proc_stream(self):
+        """
+        Returns a list of 3 booleans: [fft, interp, 32bit or 64bit]
+        """
+        return [self.fft_check.checked(), self.interp_check.checked(), self.bitness_check.checked()]
+
+    def get_config(self):
+        return self.config_combo.text()
+
+    def get_rate(self):
+        return Rates[self.rate_combo.text()]
+
+    def get_apod(self):
+        return self.apod_combo.text()
+
+class RepeatsPanel(QGroupBox):
+
+    def __init__(self):
+
+        super(QGroupBox, self).__init__()
+        ui = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))+"\\ui\\repeats.ui"  # Double escape dir
+        uic.loadUi(ui, self)
+
+        self.a_repeat_spin = self.findChild(QSpinBox, "spinARepeat")
+        self.b_repeat_spin = self.findChild(QSpinBox, "spinBRepeat")
+        self.avg_check = self.findChild(QCheckBox, "checkAveraging")
+
+        self.show()
+
+    def get_a_repeat(self):
+
+        if self.isEnabled():
+            return self.a_repeat_spin.value()
+        else:
+            return 1
+
+    def get_b_repeat(self):
+
+        if self.isEnabled():
+            return self.b_repeat_spin.value()
+        else:
+            return 1
+
+    def averaging(self):
+
+        if self.isEnabled():
+            return self.avg_check.checked()
+        else:
+            return False
+
+
+class FilePanel(QGroupBox):
+
+    def __init__(self):
+
+        super(QGroupBox, self).__init__()
+
+        ui = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))+"\\ui\\filePanel.ui"  # Double escape dir
+        uic.loadUi(ui, self)
+
+        self.exp_dir_line = self.findChild(QLineEdit, "lineExp")
+        self.trial_name_line = self.findChild(QLineEdit, "lineFileName")
+        self.file_type_combo = self.findChild(QComboBox, "comboFileType")
+        self.file_size_combo = self.findChild(QComboBox, "comboFileSize")
+
+        self.file_browse_button = self.findChild(QToolButton, "buttonBrowse")
+        self.file_browse_button.clicked.connect(self.browse_for_file)
+
+        self.file_dialog = QFileDialog()
+
+        # Defaults.  All fields managed by Qt elements themselves for now
+
+        self.default_exp_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "\\exp-" + datetime.today().strftime('%Y-%m-%d')
+        self.exp_dir_line.setText(self.default_exp_dir)
+
+        self.default_trial_name = "rec01"
+        self.trial_name_line.setText(self.default_trial_name)
+
+        self.file_type_combo.setCurrentIndex(0)
+
+        self.file_type_combo.setCurrentIndex(1)
+
+        self.show()
+
+    def browse_for_file(self):
+        #  Uses QFileDialog to select a directory to save in
+        self.exp_dir_line.setText(self.file_dialog.getExistingDirectory(self, "Select Directory"))
+
+    # Getters
+
+    def get_experiment_directory(self):
+        return self.exp_dir_line.text()
+
+    def get_trial_name(self):
+        return self.trial_name_line.text()
+
+    def get_file_type(self):
+        return str(self.file_type_combo.curentText())
+
+    def get_file_size(self):
+        return str(self.file_size_combo.curentText())
