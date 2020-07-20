69b10958d648de472aaf4ea50e3f9c0272701ca7
sstucker <sstucker@bu.edu>
2020-07-19 21:54:21 -0400

    pyinstaller hook

---
+++ target/PyInstaller/fbs_pyinstaller_hook.py
@@ -0,0 +1,3 @@
+import importlib
+module = importlib.import_module('fbs_runtime._frozen')
+module.BUILD_SETTINGS = {'app_name': 'PyImageOCT', 'author': 'Stephen Tucker', 'version': '0.0.0', 'environment': 'local'}
